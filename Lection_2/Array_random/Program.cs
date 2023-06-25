f69fb3af9ed157f7915100ca5c69443f8982444c
Dmitry <cuz1301@gmail.com>
2023-06-25 18:27:32 +0300

    feat: 3rd task done

---
+++ Lection_2/Array_random/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+       collection[index] = new Random().Next(1, 20);
+        index++;
+    }  
+}
+void printArray(int[] col)
+{
+ int count = col.Length;
+ int position = 0;
+ while (position < count )
+ {
+    Console.Write(col[position]+",");
+    position++;
+
+ }
+}
+
+int indexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count )
+    {
+        if (collection [index] == find)
+        {
+             position = index;
+             break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[100];
+FillArray(array);
+printArray(array);
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+int pos = indexOf(array, 11);
+
+Console.WriteLine (pos);
