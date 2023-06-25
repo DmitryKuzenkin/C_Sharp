e6bd4774a73f585a4b9a5d481a3f1d639f4253c3
Dmitry <cuz1301@gmail.com>
2023-06-25 20:30:23 +0300

    fix prompt fix

--- Lesson_1/Homework_0.8/Program.cs
+++ Lesson_1/Homework_0.8/Program.cs
@@ -2,11 +2,10 @@
 // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
-
+ Console.WriteLine("Insert the number");
 int a = int.Parse(Console.ReadLine()!);
 int count = 2;
 
-Console.WriteLine("Insert the number");
 
 while (count <=a )
 {
