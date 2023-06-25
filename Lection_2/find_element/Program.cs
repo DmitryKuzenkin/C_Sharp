98bb6a2418de947ae9c19abb531a40a969b37753
Dmitry <cuz1301@gmail.com>
2023-06-25 17:47:27 +0300

    feat: second task add

---
+++ Lection_2/find_element/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = {13,276,3,444,53,436,27,568,98,};
+
+int n = array.Length;
+int find = 4;
+int index = 0;
+
+while (index < n)
+{
+    if (find == array[index])
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+
+}
+
