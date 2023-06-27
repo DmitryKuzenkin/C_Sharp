ceaf38cbb48616c78c098105ac29c40b60a48710
Dmitriy <cuz1301@gmail.com>
2023-06-27 12:18:46 +0300

    feat: 1st task done

---
+++ Lesson_2/HW_10/Program.cs
@@ -0,0 +1,14 @@
+﻿void Num2 (int num)
+{
+    if (10 <= num && num < 1000)
+    {
+        num = (num / 10) % 10;
+        Console.WriteLine ("2 цифра числа: " + num);
+    }
+    else 
+    {
+        Console.WriteLine ("Число не трехзначное");
+    }
+}
+int NumA = int.Parse(Console.ReadLine());
+Num2(NumA);
