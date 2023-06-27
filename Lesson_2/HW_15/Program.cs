50b4ccdc99afd73b6e6ab06dbeaafaab7a18e642
Dmitriy <cuz1301@gmail.com>
2023-06-27 12:58:04 +0300

    feat: 3rd task done

---
+++ Lesson_2/HW_15/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = int.Parse(Console.ReadLine())!;
+if (a == 6 || a == 7)
+{
+    Console.WriteLine("Выходной");
+}
+else if (a > 7)
+{
+    Console.WriteLine("Такого дня недели нет");
+}
+else 
+{
+    Console.WriteLine("Будний");
+}
+
