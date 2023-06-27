38951052fc02714787dd7abf8dac4d590921c8d6
Dmitriy <cuz1301@gmail.com>
2023-06-27 13:05:59 +0300

    fix: 1st task final fix

--- Lesson_2/HW_10/Program.cs
+++ Lesson_2/HW_10/Program.cs
@@ -1,6 +1,6 @@
 ﻿void Num2 (int num)
 {
-    if ( 10 <= num && num < 1000)
+    if ( 10 < num && num < 1000)
     {
         num = (num / 10) % 10;
         Console.WriteLine ("2 цифра числа: " + num);
