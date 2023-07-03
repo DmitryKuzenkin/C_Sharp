21e84a891f30e95f11bb83b87e871a7d24556c1f
Dmitriy <cuz1301@gmail.com>
2023-07-03 20:45:59 +0300

    feat: 1st task done

---
+++ Lesson_4/0.1/Program.cs
@@ -0,0 +1,12 @@
+﻿int Summ(int a)
+{ 
+    int sum = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+int num = Summ(int.Parse(Console.ReadLine()!));
+Console.WriteLine($"->{num}");
+
