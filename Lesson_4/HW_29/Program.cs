c976949b113f484599b5480d104e9fe52f277f4a
Dmitriy <cuz1301@gmail.com>
2023-07-08 11:03:30 +0300

    feat: 29 task done

---
+++ Lesson_4/HW_29/Program.cs
@@ -0,0 +1,17 @@
+﻿void FillArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+       collection[i] = new Random().Next(0,100);
+    }  
+}
+void printArray(int[] col)
+{
+ for (int i = 0; i < col.Length - 1; i++)
+   {
+      Console.Write(col[i]+",");
+   }
+}
+int[] array = new int[8];
+FillArray(array);
+printArray(array);
