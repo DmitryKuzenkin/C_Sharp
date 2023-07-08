4abd0cd5c925d0964270fef730bfcbcb74d48e5e
Dmitriy <cuz1301@gmail.com>
2023-07-08 10:31:17 +0300

    feat 25 task done

---
+++ Lesson_4/HW_25/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите степень: ");
+int b = int.Parse(Console.ReadLine()!);
+int Result = 1;
+
+for (int i = 0; i < b; i++)
+{
+    
+    Result = Result * a;
+
+}
+
+Console.WriteLine(Result);
+
