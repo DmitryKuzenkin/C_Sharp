054db2b1d9d959313ef3820532e560b0d95de8a0
Dmitriy <cuz1301@gmail.com>
2023-07-08 13:39:26 +0300

    feat: 4th lection add

---
+++ Lection_4/0.1/Program.cs
@@ -0,0 +1,47 @@
+﻿/*string[,] table = new string[2,5];
+table [1,2] = "Слово";
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int col = 0; col < 5; col++)
+    {
+        Console.WriteLine($"-{table[rows,col]}-");
+    }
+}
+
+Console.WriteLine();*/
+
+void PrintArray(int[,] arr)
+
+
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                 Console.Write($"|{arr[i,j]}|");
+            }
+    Console.WriteLine();
+    }
+}
+int[,] MassNums(int from, int to)
+{
+    int col = new Random().Next(1,11);
+    int rows = new Random().Next(1,11);
+    int[,] arr = new int[col,rows];
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = new Random().Next(from, to + 1);
+        }
+    }
+    return arr;
+}
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+int[,] mass = new int[10,10];
+PrintArray (mass);
+Console.WriteLine();
+mass = MassNums(start, stop);
+PrintArray (mass);
---
+++ Lection_4/0.3/Program.cs
@@ -0,0 +1,10 @@
+﻿double F(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return F(n-1) + F(n-2);
+
+}
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"i{i} = {F(i)}");
+}
