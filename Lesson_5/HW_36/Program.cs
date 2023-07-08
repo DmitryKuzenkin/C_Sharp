6f7730283756db6ffdb9d3f36dc94e78264c05bc
Dmitriy <cuz1301@gmail.com>
2023-07-08 11:42:38 +0300

    feat: 36 task done

---
+++ Lesson_5/HW_36/Program.cs
@@ -0,0 +1,40 @@
+﻿void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to + 1);
+    return arr;
+}
+
+int NEvenSum(int[] col)
+{
+    int n = 0;
+    for (int i = 1; i < col.Length; i+=2)   
+    {
+        n = n + col[i];
+    }
+    return n;
+}
+
+
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+int result = NEvenSum(mass);
+Console.WriteLine(result);
