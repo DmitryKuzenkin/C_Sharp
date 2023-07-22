26bfb3621f8ef01eedcd176422dc30e55a07234d
Dmitriy <cuz1301@gmail.com>
2023-07-22 19:09:25 +0300

    fix 1st task

--- Lesson_9/Task_1/Program.cs
+++ Lesson_9/Task_1/Program.cs
@@ -1,18 +1,9 @@
-﻿
-void EvenNum(int M, int N)
+﻿void EvenNum(int M, int N)
 {
-    if (M > 0)
-    {
-        Console.Write($"{M},");
-        M = M + 2;
-        if (M <= N)
-            EvenNum (M, N); 
-    } 
-    else Console.WriteLine("Промежуток неверный");
+    if (N < M) return;
+    EvenNum(M, N - 1);
+    if (N % 2 == 0)
+    Console.Write($"{N}, ");
 }
 
-Console.Write($"M->\t");
-int M = int.Parse(Console.ReadLine()!);
-Console.Write($"N->\t");
-int N = int.Parse(Console.ReadLine()!);
-EvenNum(M, N);
+EvenNum(4, 8);
